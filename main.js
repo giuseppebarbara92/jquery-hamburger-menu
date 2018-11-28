2e86708379f4241742ac588dae63a1d00276c5cd
giuseppe.barbara92 <giuseppe.barbara1992@gmail.com>
2018-11-28 02:12:57 +0100

    Hamburger_menu html/css/js

---
+++ main.js
@@ -0,0 +1,9 @@
+ // Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript.
+
+$('#hamburger').click(function(){
+$('.hamburger_menu').show(500);
+});
+
+$('#cross').click(function(){
+$('.hamburger_menu').hide(500);
+});
